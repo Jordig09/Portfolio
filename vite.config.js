ea1e7cf73c2ffadfe699e8865ee3bd07d824708c
Jordi Godoy <62894301+Jordig09@users.noreply.github.com>
2024-06-03 20:39:54 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NOMBRE_DEL_REPOSITORIO/',
+  base: '/Portfolio/',
   plugins: [react()],
 })
