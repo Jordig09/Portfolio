a49287edf20d123e561d0dd73594210d568ffd9f
Jordig09 <jordii09.jg@gmail.com>
2024-06-03 22:59:22 -0300

    correción importaciones

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,5 @@
 function ProjectCard(props) {
-  const imgUrl = `src/assets/img/projects/${props.projectImg}-0.jpg`;
+  const imgUrl = `/Portfolio/img/projects/${props.projectImg}-0.jpg`;
   // const url = `src/assets/img/projects/digital-hard-dashboard.jpg`;
   return (
     <div
