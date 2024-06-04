78b4d7e7036f72f46affbaa1e9a89ea87fdb9be1
Jordi Godoy <62894301+Jordig09@users.noreply.github.com>
2024-06-03 22:38:12 -0300

    Update Room.jsx

--- src/models/Room.jsx
+++ src/models/Room.jsx
@@ -48,7 +48,7 @@ export function Room(props) {
 
   const notebookTexture = useLoader(
     TextureLoader,
-    "Portfolio/src/assets/img/notebook-material.jpg"
+    "src/assets/img/notebook-material.jpg"
   );
   notebookTexture.center.set(0.17, 0.33);
   notebookTexture.repeat.set(4, 4);
