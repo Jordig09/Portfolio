d6f4705a05261778aea38f52e0d7aac897575b65
Jordi Godoy <62894301+Jordig09@users.noreply.github.com>
2024-06-03 21:16:59 -0300

    Update Room.jsx

--- src/models/Room.jsx
+++ src/models/Room.jsx
@@ -10,7 +10,7 @@ import RightMonitor from "../components/RightMonitor";
 import { Context } from "../components/App";
 
 export function Room(props) {
-  const { nodes, materials } = useGLTF("/Portfolio/public/room.glb");
+  const { nodes, materials } = useGLTF("/public/room.glb");
 
   const { sectionActive } = useContext(Context);
 
@@ -966,4 +966,4 @@ export function Room(props) {
   );
 }
 
-useGLTF.preload("/Portfolio/public/room.glb");
+useGLTF.preload("/public/room.glb");
