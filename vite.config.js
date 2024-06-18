a79a503f840112a9dc3d281eb7eb6e44fa07a113
Jordi Godoy <62894301+Jordig09@users.noreply.github.com>
2024-06-18 19:39:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "https://Jordig09.github.io/Portfolio/",
   plugins: [react()],
 });
